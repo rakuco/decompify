3ea360ea07a411d4c531aee625c7b8dd52455ab2
Raphael Kubo da Costa <kubito@gmail.com>
2008-12-01 12:08:57 -0200

    Adding more addressing modes and strings. In the end this will end up looking much like disasm's opcodes.asm

--- instr.h
+++ instr.h
@@ -27,13 +27,77 @@
 %define __INSTR_H
 
 ;; Argument types (immed, r/m etc)
-ARGTYPE_NONE      db    1<<0
-ARGTYPE_CONST1    db    1<<1
-ARGTYPE_CONST3    db    1<<2
-ARGTYPE_IMMED     db    1<<3
-ARGTYPE_MEMORY    db    1<<4
-ARGTYPE_REGMEM    db    1<<5
-ARGTYPE_REGISTER  db    1<<6
+ARGTYPE_NONE      db    0
+ARGTYPE_CONST1    db    1
+ARGTYPE_CONST3    db    2
+ARGTYPE_REGAL     db    3
+ARGTYPE_REGCL     db    4
+ARGTYPE_REGDL     db    5
+ARGTYPE_REGBL     db    6
+ARGTYPE_REGAH     db    7
+ARGTYPE_REGCH     db    8
+ARGTYPE_REGDH     db    9
+ARGTYPE_REGBH     db    10
+ARGTYPE_REGAX     db    11
+ARGTYPE_REGCX     db    12
+ARGTYPE_REGDX     db    13
+ARGTYPE_REGBX     db    14
+ARGTYPE_REGSP     db    15
+ARGTYPE_REGBP     db    16
+ARGTYPE_REGSI     db    17
+ARGTYPE_REGDI     db    18
+ARGTYPE_REGES     db    19
+ARGTYPE_REGCS     db    20
+ARGTYPE_REGSS     db    21
+ARGTYPE_REGDS     db    22
+ARGTYPE_IMMED     db    23
+ARGTYPE_MEMORY    db    24
+ARGTYPE_REGMEM    db    25
+ARGTYPE_REGISTER  db    26
+
+;; Strings for the arguments that need it
+SARGTYPE_CONST1     db    "1", 0
+SARGTYPE_CONST3     db    "3", 0
+SARGTYPE_REGAL      db    "al", 0
+SARGTYPE_REGCL      db    "cl", 0
+SARGTYPE_REGDL      db    "dl", 0
+SARGTYPE_REGBL      db    "bl", 0
+SARGTYPE_REGAH      db    "ah", 0
+SARGTYPE_REGCH      db    "ch", 0
+SARGTYPE_REGDH      db    "dh", 0
+SARGTYPE_REGBH      db    "bh", 0
+SARGTYPE_REGAX      db    "ax", 0
+SARGTYPE_REGCX      db    "cx", 0
+SARGTYPE_REGDX      db    "dx", 0
+SARGTYPE_REGBX      db    "bx", 0
+SARGTYPE_REGSP      db    "sp", 0
+SARGTYPE_REGBP      db    "bp", 0
+SARGTYPE_REGSI      db    "si", 0
+SARGTYPE_REGDI      db    "di", 0
+SARGTYPE_REGES      db    "es", 0
+SARGTYPE_REGCS      db    "cs", 0
+SARGTYPE_REGSS      db    "ss", 0
+SARGTYPE_REGDS      db    "ds", 0
+SARGTYPE_RM_BX_SI   db    "bx+si", 0
+SARGTYPE_RM_BX_DI   db    "bx+di", 0
+SARGTYPE_RM_BP_SI   db    "bx+si", 0
+SARGTYPE_RM_BP_DI   db    "bx+si", 0
+SARGTYPE_RM_SI      db    "si", 0
+SARGTYPE_RM_DI      db    "di", 0
+SARGTYPE_RM_BP      db    "bp", 0
+SARGTYPE_RM_BX      db    "bx", 0
+
+;; Pointers for groups of strings
+ARRAY_CONSTARGS     dd    SARGTYPE_CONST1, SARGTYPE_CONST2
+ARRAY_8BITREGS      dd    SARGTYPE_REGAL, SARGTYPE_REGCL, SARGTYPE_REGDL
+                    dd    SARGTYPE_REGBL, SARGTYPE_REGAH, SARGTYPE_REGCH
+                    dd    SARGTYPE_REGDH, SARGTYPE_REGBH
+ARRAY_16BITREGS     dd    SARGTYPE_REGAX, SARGTYPE_REGCX, SARGTYPE_REGDX
+                    dd    SARGTYPE_REGBX, SARGTYPE_REGSP, SARGTYPE_REGBP
+                    dd    SARGTYPE_REGSI, SARGTYPE_REGDI
+ARRAY_RM_MODES      dd    SARGTYPE_RM_BX_SI, SARGTYPE_RM_BX_DI, SARGTYPE_RM_BP_SI
+                    dd    SARGTYPE_RM_BP_DI, SARGTYPE_RM_SI, SARGTYPE_RM_DI
+                    dd    SARGTYPE_RM_BP, SARGTYPE_RM_BX
 
 ;; Instruction list (for now, here)
 I_ADD             db    "add", 0
